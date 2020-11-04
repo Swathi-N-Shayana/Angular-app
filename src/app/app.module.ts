7f663930961c87f14e09da0be4854b0a9691dbc2
Swathi N Shayana <55015685+Swathi-N-Shayana@users.noreply.github.com>
2020-11-04 23:04:18 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstComponent } from './components/first/first.component';
 import { FormsModule } from '@angular/forms';
 import { MovieComponent } from './component/movie/movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent,
     MovieComponent
   ],
   imports: [
